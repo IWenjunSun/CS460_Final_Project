44e271f03145ccc3618387ece624168833f54a5a
jjmarsz2 <jjmarsz2@illinois.edu>
2018-04-18 13:37:31 -0500

    started aimbot work

--- readmem/readmem/main.cpp
+++ readmem/readmem/main.cpp
@@ -7,22 +7,17 @@
 #pragma comment(lib, "psapi.lib")
 
 using namespace std;
-DWORD base = 0x905a4d;
-DWORD player_offset = 0x11069bc;
-DWORD hp_offset = 0x1e0;
-DWORD base_addr;
-//DWORD* ptr = base;
-DWORD hp = 0x11069bc;
-float myhp = 100;
-
-
-int myammo;
+const DWORD player_offset = 0x3D24D4;
+const DWORD x_rot_off = 0x326EC0;
+const DWORD y_rot_off = 0x326EBC;
+const DWORD entity_diff = 0x10;
+const DWORD player_x_off = 0x348;
+const DWORD player_y_off = 0x34C;
+const DWORD player_z_off = 0x350;
 
 
 
-
-HMODULE GetBaseAddressByName(DWORD processId, TCHAR *processName)
-{
+HMODULE GetBaseAddressByName(DWORD processId, TCHAR *processName){
 	TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
 
 	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
@@ -46,7 +41,12 @@ HMODULE GetBaseAddressByName(DWORD processId, TCHAR *processName)
 
 int main() {
 
-	HWND hwnd = FindWindowA(NULL, "Counter-Strike");
+
+
+
+
+
+	HWND hwnd = FindWindowA(NULL, "Counter-Strike Source");
 	if (hwnd == NULL) {
 		cout << "Cannot find window" << endl;
 		Sleep(3000);
@@ -64,12 +64,31 @@ int main() {
 			exit(-1);
 		}
 		else {
-			HMODULE addr = GetBaseAddressByName(procID, (TCHAR*)"hl.exe");
+			HMODULE addr = GetBaseAddressByName(procID, (TCHAR*)"hl2.exe");
+			DWORD engine_offset = (DWORD)addr + 0x1FC00000;
+			DWORD server_offset = (DWORD)addr + 0x21C00000;
+			DWORD player_addr;
+			int num_players;
+			float x_rot, y_rot;
+			float player_x, player_y, player_z;
+			//get number of players, excluding self
+			ReadProcessMemory(handle, (LPVOID)(engine_offset + 0x77BB94), &num_players, sizeof(num_players), 0);
+			num_players--;
+			//main aimbot while loop
 			while (1) {
-				ReadProcessMemory(handle, (LPVOID)((DWORD)addr + 0x11069bc), &base_addr, sizeof(base_addr), 0);
-				WriteProcessMemory(handle, (LPVOID)(base_addr + 0x1e0), &myhp, sizeof(myhp), 0);
-				cout << base_addr << endl;
-				Sleep(100);
+				//get player rotation
+				ReadProcessMemory(handle, (LPVOID)(engine_offset+x_rot_off), &x_rot, sizeof(x_rot), 0);
+				ReadProcessMemory(handle, (LPVOID)(engine_offset + y_rot_off), &y_rot, sizeof(y_rot), 0);
+				//get player position
+				ReadProcessMemory(handle, (LPVOID)(server_offset + player_offset), &player_addr, sizeof(player_addr), 0);
+				ReadProcessMemory(handle, (LPVOID)(player_addr + player_x_off), &player_x, sizeof(player_x), 0);
+				ReadProcessMemory(handle, (LPVOID)(player_addr + player_y_off), &player_y, sizeof(player_y), 0);
+				ReadProcessMemory(handle, (LPVOID)(player_addr + player_z_off), &player_z, sizeof(player_z), 0);
+
+				/*for (float i = -89; i < 90; i++) {
+					WriteProcessMemory(handle, (LPVOID)(engine_offset + y_rot_off), &i, sizeof(i), 0);
+					Sleep(10);
+				}*/
 			}
 		}
 	}
